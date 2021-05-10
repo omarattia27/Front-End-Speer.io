7b6b7eee9b8aaf76c0e20e5587960e787b4d280b
omarattia27 <70349770+omarattia27@users.noreply.github.com>
2021-05-10 15:47:47 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import SlideShow from './SlideShow.js';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header>
+        <a>
+          |||
+        </a>
+        <div>
+          item1
+        </div>
+        <div>
+          item2
+        </div>
+        <div>
+          item3
+        </div>
+        <div>
+          <button>
+            Shopping Basket
+          </button>
+        </div>
+      </header>
+      <SlideShow />
+
+    </div>
+  );
+}
+
+export default App;
